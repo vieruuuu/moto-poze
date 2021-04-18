211f5a61064dfa64137b61b0ba9d7eb5e9bd2af0
vieru <mixerandr3w001@gmail.com>
2021-04-18 11:17:40 +0300

    cam gata, trebuie sa repar sa puna numele la fisier si sa mai modific modelul asta

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const puppeteer = require("puppeteer");
 const path = require("path");
+const { watch } = require("fs");
+const { readFile } = require("fs/promises");
 
-async function screenshot(browser, name, data) {
+async function getData(dataFile) {
+  const rawData = await readFile(dataFile, { encoding: "utf-8" });
+  let dataArray = [];
+  let fileName = "";
+
+  const linii = rawData.split("\n");
+  const len = linii.length;
+
+  for (let i = 0; i < len; i++) {
+    const linie = linii[i];
+
+    if (linie.includes(":::")) {
+      const slot = linie.split(":::")[1];
+
+      dataArray.push(slot);
+    } else if (linie.includes("===")) {
+      fileName = "gen0" + linie.split("===")[1].replace(" ", "0");
+    }
+  }
+
+  return { fileName, dataArray };
+}
+
+async function screenshot(browser, name, dataArray) {
   const page = await browser.newPage();
 
   await page.goto(path.join(__dirname, "index.html"), {
@@ -10,31 +35,59 @@ async function screenshot(browser, name, data) {
 
   await page.waitForSelector("#tabelul");
 
-  await page.evaluate(() => {
+  await page.evaluate((dataArray) => {
     const body = document.body;
+    const len = dataArray.length;
 
-    for (let i = 0; i < 20; i++) {
-      body.innerHTML = body.innerHTML.replace(`slot${i}`, `replaced${i}`);
+    for (let i = 0; i < len; i++) {
+      const slot = dataArray[i];
+
+      body.innerHTML = body.innerHTML.replace(`slot${i}`, slot);
     }
-  });
+  }, dataArray);
 
   const element = await page.$("#tabelul");
 
+  console.log(`photos/${name}`);
+
   await element.screenshot({
-    path: `photos/${name}.png`,
+    path: `photos/gen00ceva.png`,
   });
 
   await page.close();
 }
 
-async function main() {
+function watchForData(browser) {
+  const dataFile = "data.txt";
+  let timeout;
+
+  const watcher = watch(dataFile, async (eventName) => {
+    if (!timeout) {
+      timeout = setTimeout(() => (timeout = null), 5000); // 5s, sa nu se faca de mai multe ori un ss daca ai salvat o data
+
+      if (eventName === "change") {
+        console.log("citesc datele");
+        const { fileName, dataArray } = await getData(dataFile);
+        console.log("gata datele");
+
+        console.log("creez poza");
+        await screenshot(browser, fileName, dataArray);
+        console.log("poza gata");
+      }
+    }
+  });
+}
+
+(async () => {
+  console.log("started");
+
+  console.log("starting puppeteer");
   const browser = await puppeteer.launch({
     // headless: false,
   });
 
-  await screenshot(browser, "example");
+  console.log("watching data.txt");
+  watchForData(browser);
 
-  await browser.close();
-}
-
-main();
+  // await browser.close();
+})();
